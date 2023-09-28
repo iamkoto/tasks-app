ff4b9eebea553bfc5f7c2f70de2fd20fb4564961
iamkoto <134742316+iamkoto@users.noreply.github.com>
2023-09-27 22:26:34 -0300

    aaaa

--- Assets/index.js
+++ Assets/index.js
@@ -6,21 +6,32 @@ document.addEventListener('DOMContentLoaded', function () {
         const nome = document.getElementById('nomeInput').value;
         const idade = parseInt(document.getElementById('idadeInput').value);
         const cidade = document.getElementById('cidadeInput').value;
+        const sangue = document.getElementById('sangueInput').value;
         
-        if (nome && !isNaN(idade) && cidade) {
+        if (nome && !isNaN(idade) && cidade && sangue) {
             // cria um objeto pessoa com os dados
             const pessoa = {
                 nome: nome,
                 idade: idade,
                 cidade: cidade,
+                sangue: sangue,
             };
 
             // adiciona a pessoa ao JSON
             adicionarPessoa(pessoa);
-        } else {
-            alert('Preencha todos os campos corretamente');0
+        } else if  (!nome) {
+            alert('Preencher o campo nome');
+        } else if (!idade) {
+            alert('Idade inválida');
+        } else if (!cidade) {
+            alert('Dados da cidade inválido');
+        } else if(!sangue) {
+            alert('Tipo de sangue inválido');
+        } 
+         else {
+            alert('Preencha todos os campos corretamente');
         }
-    })
+    });
     function adicionarPessoa(pessoa) {
         const jsonAtual = {
             pessoas: []
@@ -49,6 +60,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("nomeInput").value = '';
         document.getElementById("idadeInput").value = '';
         document.getElementById("cidadeInput").value = '';
+        document.getElementById("sangueInput").value = '';
 
         atualizarListaPessoas(jsonAtual.pessoas);
     }
@@ -57,22 +69,54 @@ document.addEventListener('DOMContentLoaded', function () {
         listaPessoas.innerHTML = '';
 
         // adiciona pessoas a lista
-        pessoas.forEach(function (pessoa) {
+        pessoas.forEach(function (pessoa, index) {
             const li = document.createElement('li');
-            li.textContent = `Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Cidade: ${pessoa.cidade}`;
+            li.textContent = `Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Cidade: ${pessoa.cidade}, Sangue: ${pessoa.sangue}`;
             listaPessoas.appendChild(li);
+
+            const removeButton = document.createElement('button')
+
+            removeButton.textContent = 'Remover pessoas';
+            li.appendChild(removeButton)
+            
+            // Remover pessoas por pessoa da lista
+            removeButton.addEventListener('click', function() {
+                removerPessoa(index)
+            });
         })
-        
     }
-    const jsonArmazenado = localStorage.getItem('dadosJson');
-    if (jsonArmazenado) {
-        try {
-            const parsedJson = JSON.parse(jsonArmazenado);
-            if (Array.isArray(parsedJson.pessoas)) {
-                atualizarListaPessoas(parsedJson.pessoas);
+    
+    
+    function removerPessoa(index) {
+        const jsonArmazenado = localStorage.getItem('dadosJson');
+
+        if (jsonArmazenado) {
+            try {
+                const parsedJson = JSON.parse(jsonArmazenado);
+                if (Array.isArray(parsedJson.pessoas)) {
+                    // Remove a pessoa pelo índice
+                    parsedJson.pessoas.splice(index, 1);
+
+                    // Atualiza o JSON no localStorage
+                    localStorage.setItem('dadosJson', JSON.stringify(parsedJson));
+
+                    // Atualiza a lista de pessoas na página
+                    atualizarListaPessoas(parsedJson.pessoas);
+                }
+            } catch (error) {
+                console.error('erro ao analizar JSON', error);
             }
-        } catch (error) {
-            console.error('erro ao analizar JSON', error);
-        }
+        };
     }
-})
+
+    
+
+
+    // Botão que apaga toda lista criada anteriormente
+    const limpaLista = document.getElementById('limpaLista')
+
+    limpaLista.addEventListener('click', function (){
+        localStorage.clear();
+        listaPessoas.innerHTML = '';
+    });
+});
